9278e5f63b1e28481e79486aa920d7cf2b18f1b1
Tejasvin V <tejasvin.vimal@gmail.com>
2024-11-18 20:19:05 +0530

    Initial Commit

---
+++ spring-angular-registration/src/main/java/com/wissensalt/test/sar/SpringAngularRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.wissensalt.test.sar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularRegistrationApplication.class, args);
+	}
+
+}
